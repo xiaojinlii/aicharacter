f69c69a011d37b4c5f760e401ae3d78aef9218e7
JinLiGame <553555614@qq.com>
2024-03-19 17:41:19 +0800

    aicharacter配置

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from modules.quickstart.routes import router as quickstart_router
+# from modules.quickstart.routes import router as quickstart_router
+from modules.aicharacter.routes import router as aicharacter_router
 
 
 def register_routes(app: FastAPI):
@@ -8,4 +9,5 @@ def register_routes(app: FastAPI):
     注册路由
     """
 
-    app.include_router(quickstart_router, prefix="/quickstart", tags=["快速开始"])
+    # app.include_router(quickstart_router, prefix="/quickstart", tags=["快速开始"])
+    app.include_router(aicharacter_router, prefix="/character", tags=["角色管理"])
--- application/settings.py
+++ application/settings.py
@@ -11,8 +11,8 @@ DEBUG = False
 
 
 """项目配置"""
-TITLE = "FastAPI Framework"
-DESCRIPTION = "基于FastAPI的Web框架"
+TITLE = "AI Character"
+DESCRIPTION = "基于FastAPI的ai角色对话"
 VERSION = "0.0.1"
 
 
